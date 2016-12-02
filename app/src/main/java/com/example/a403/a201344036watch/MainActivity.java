11edf790024cad1d5f17c12cf6f311a8585478a9
Sungwoo <ykk2006@naver.com>
2016-12-02 14:51:10 +0900

    2. [예약시작] 버튼을 클릭하면 그 위의 Chronometer가 작동한다.

--- app/src/main/java/com/example/a403/a201344036watch/MainActivity.java
+++ app/src/main/java/com/example/a403/a201344036watch/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.a403.a201344036watch;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chm1;
+    CalendarView cdv1;
+    Button btn1, btn2;
+    RadioButton rbtn1, rbtn2;
+    TimePicker tp1;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chm1 = (Chronometer) findViewById(R.id.chm1);
+        cdv1 = (CalendarView) findViewById(R.id.calendarView);
+        btn1 = (Button) findViewById(R.id.btn);
+        btn2 = (Button) findViewById(R.id.btn2);
+        rbtn1 = (RadioButton) findViewById(R.id.rbtn1);
+        rbtn2 = (RadioButton) findViewById(R.id.rbtn2);
+        tp1 = (TimePicker) findViewById(R.id.timePicker);
+        tv1 = (TextView) findViewById(R.id.tv1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chm1.setBase(SystemClock.elapsedRealtime());
+                chm1.start();
+                chm1.setTextColor(Color.RED);
+            }
+        });
     }
 }
